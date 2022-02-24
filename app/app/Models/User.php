5bf658a1d525206b8bddf65de54c39681cb52abd
vautrin33u <vautrin33u@etu.univ-lorraine.fr>
2022-02-24 16:51:25 +0100

    Migrations

--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -2,11 +2,12 @@
 
 namespace App\Models;
 
+use App\Models\Evenement;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
@@ -21,6 +22,11 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'prenom',
+        'photo',
+        'numeroTelephone',
+        'notificationMail',
+        'dateNaissance'
     ];
 
     /**
@@ -40,5 +46,22 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'datenaissance' => 'datetime',
     ];
+
+    /**
+     * Retourne la localisation associé à un utilisateur
+     */
+    public function localisation()
+    {
+        return $this->belongsTo(Localisation::class);
+    }
+
+    /**
+     * 
+     */
+    public function evenement()
+    {
+        return $this->belongsToMany(Evenement::class);
+    }
 }
--- app/database/migrations/2014_10_12_000000_create_users_table.php
+++ app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,10 +1,11 @@
 <?php
 
+use App\Models\Localisation;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +16,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('name', 50);
+            $table->string('prenom', 50);
+            $table->string('photo', 50);
+            $table->string('numeroTelephone', 12);
+            $table->boolean('notificationMail');
+            $table->date('dateNaissance');
+            $table->string('email', 320)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('localisation_id')->references('id')->on('localisations');
+
         });
     }
 
@@ -33,4 +41,4 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
     }
-};
+}
