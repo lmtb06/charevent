1ae44d7f6555b167dd879ff01de140e0e24022d0
Guillaume V <obsywon@gmail.com>
2022-02-26 21:07:46 +0100

    Ajout des routes et controllers ajout des fonctions dans controllers (mais peut-être erreurs dans les noms car je n'avais pas accès au diagramme UML :'))

---
+++ src/app/Http/Controllers/InscriptionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class InscriptionController extends Controller
+{
+
+    public function create (){
+
+
+    }
+    public function store (Request $request){
+
+    }
+}
