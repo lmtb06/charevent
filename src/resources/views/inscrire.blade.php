717a9f03ba42dbaffcf36a7069689c32e4c35864
alex <alexandre.serratore@gmail.com>
2022-03-06 16:10:53 +0100

    .

--- src/resources/views/inscrire.blade.php
+++ src/resources/views/inscrire.blade.php
@@ -7,11 +7,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{asset('/css/style.css')}}">
-
     </head>
-
     <body>
-        @yield('content')
+        <!--Fichier du bandeau commun en haut-->
+        @include('layout.barreMenu1', ['user' => $user ?? ''])
+        <!--Fichier qui contient le formulaire-->
+        @yield('content')        
     </body>
 
 </html>
