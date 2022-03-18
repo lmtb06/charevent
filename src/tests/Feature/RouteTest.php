d275be2b549acea550d1ed6a9ffeddcbf77a9009
wehddn <nigraff@yandex.ru>
2022-03-18 12:22:10 +0100

    Modification minime de RouteTest

--- src/tests/Feature/RouteTest.php
+++ src/tests/Feature/RouteTest.php
@@ -21,7 +21,7 @@ class RouteTest extends TestCase
 
         $response->assertStatus(200);
     }
-    /*
+
     public function test_post_evenement_update_1()
     {
         $evenement = Evenement::findOrFail(1);
@@ -29,7 +29,7 @@ class RouteTest extends TestCase
 
         $response->assertStatus(200);
     }
-    */
+
     public function test_post_evenement_delete_1()
     {
         $evenement = Evenement::findOrFail(1);
