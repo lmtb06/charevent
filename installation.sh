0a6ac223b24dc1c1adbbe9174ddf033be0e783f1
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2022-03-23 20:39:51 +0100

    Mise à jour installation: - Mise à jour du script d'installation

--- installation.sh
+++ installation.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Copie du code github
-printf '\n--Clonage du code github--\n\n'
-git clone git@github.com:lmtb06/charevent.git
+# # Copie du code github
+# printf '\n--Clonage du code github--\n\n'
+# git clone git@github.com:lmtb06/charevent.git
 
-# On se met dans le dossier des fichiers sources
-cd "charevent/src"
+# # On se met dans le dossier des fichiers sources
+# cd "charevent/backend"
 
 # On installe l'environnement laravel via une petite image docker
 printf "\n--Installation de l'environnement laravel--\n\n"
@@ -17,12 +17,20 @@ cp ".env.example" ".env"
 
 printf "\n--Lancement de sail--\n\n"
 ./vendor/bin/sail up -d
+./vendor/bin/sail artisan sail:install --with="mysql,redis,meilisearch,mailhog,selenium"
 
 printf "\n--Géneration de la clé laravel--\n\n"
 ./vendor/bin/sail artisan key:generate
 
 printf "\n--Installation des dépendances npm--\n\n"
 ./vendor/bin/sail npm install
+./vendor/bin/sail npm audit fix
 
-# On crée un alias pour se faciliter la
+./vendor/bin/sail down -v
+./vendor/bin/sail up -d
+
+printf "\n--Migrations BDD--\n\n"
+./vendor/bin/sail artisan migrate:fresh
+
+# On crée un alias pour se faciliter la vie
 alias sail="./vendor/bin/sail"
