5900d94218f279fd9125812b8854c73a127e1742
Romy <romane.piing@gmail.com>
2022-03-24 19:45:16 +0100

    Thème bleu ajustement

--- src/resources/views/layout/connexion.blade.php
+++ src/resources/views/layout/connexion.blade.php
@@ -25,7 +25,7 @@
 		<div class="flex justify-center h-screen">
 			<div class="hidden bg-cover bg-center bg-hand-grass lg:block lg:w-2/3 ">
 				<div class="flex items-center h-full ">
-					<div class="absolute items-center h-full lg:w-2/3 bg-gradient-to-tr opacity-40 from-logo-red  to-logo-yellow ">
+					<div class="absolute items-center h-full lg:w-2/3 bg-gradient-to-tr opacity-40 from-blue-800 to-yellow-600 ">
 					</div>
 					<div class="relative px-20">
 						<h2 class="text-4xl font-bold text-white opacity-100">CharEvent</h2>
@@ -38,7 +38,7 @@
 			<div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
 				<div class="flex-1">
 					<div class="text-center">
-						<h2 class="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-red-500 dark:text-white">CharEvent</h2>
+						<h2 class="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-yellow-600">CharEvent</h2>
 						<p class="mt-3 text-gray-500 dark:text-gray-300">Connectez-vous à CharEvent</p>
 					</div>
 
@@ -60,7 +60,7 @@
 							</div>
 
 							<div class="mt-6">
-								<input type="submit" class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-yellow-300 rounded-md hover:bg-yellow-200 focus:outline-none focus:bg-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50" value="Connexion" />
+								<input type="submit" class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50" value="Connexion" />
 							</div>
 
 						</form>
--- src/resources/views/recuperation.blade.php
+++ src/resources/views/recuperation.blade.php
@@ -9,22 +9,22 @@
 <html lang="fr">
 <body class = "bg-white">
 <!-- ici c'est la boite avec l'image -->
-<div class="bg-yellow-300 w-full h-full">
-    <div class="flex justify-center h-screen">
-        <div class="flex content-center justify-center mt-10 mb-10 px-2 rounded-full ">
-			<div class="flex items-center w-full max-w-md px-6 mx-auto bg-white rounded-xl">
+<div class="lg:bg-blue-300 w-full h-full">
+    <div class="flex justify-center h-screen ">
+        <div class="flex content-center justify-center mt-10 mb-10 px-2  rounded-full ">
+			<div class="flex lg:w-1/3 mb-10 lg:border lg:border-black lg:rounded-md px-6 mt-6 bg-white rounded-xl">
 
-				<div class="flex-1">
+				<div class="flex-1 mt-5">
                     <p class=""><a href="{{route('connexion')}}" class="text-blue-500 focus:outline-none focus:underline hover:underline">
                         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM22NlnKKa_D0qRcdKFM0ndIeDYDd548F80QO61yy-am7gXR52qeiOAASkhPQyJSeLclg&usqp=CAU" class=" h-6" alt="CharEvent"  />
                     </a></p>
 					<div class="text-center">
-						<h2 class="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-red-500 ">CharEvent</h2>
-                        <h2 class="text-xl  text-center">Récuperation de mot de passe</h2>
+						<h2 class="text-4xl font-bold text-center text-transparent bg-clip-text bg-gradient-to-r from-blue-700 to-yellow-600 ">CharEvent</h2>
+                        <h2 class="text-xl text-blue-900  text-center">Récuperation de mot de passe</h2>
             
                     </div>
-                    <p class="mt-3 text-gray-500 dark:text-gray-300">Un mot de passe temporaire vous sera envoyé par email. </p>
-                    <p class="mt-3 text-gray-500 dark:text-gray-300">Attention, si nous ne reconnaissons pas votre adresse e-mail, vous ne recevrez pas de nouveau mot de passe.</p>
+                    <p class="mt-3 text-gray-500 ">Un mot de passe temporaire vous sera envoyé par email. 
+                        Attention, si nous ne reconnaissons pas votre adresse e-mail, vous ne recevrez pas de nouveau mot de passe.</p>
                 
 
 					<div class="mt-8">
@@ -33,15 +33,15 @@
 
 							<div class="mt-6">
 								<div class="flex justify-between mb-2">
-									<label for="password" class="text-sm text-gray-600 dark:text-gray-200">Veuillez entrez votre email </label>
+									<label for="password" class="text-sm text-gray-600">Veuillez entrez votre email </label>
 								</div>
 
 								<input type="password" name="password" id="password" placeholder="mot de passe" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
 							</div>
 
-                            <div class="flex content-center justify-center mt-6">
+                            <div class="flex content-center justify-center mt-16">
                                 <input
-                                    class=" rounded-full modal-open w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-yellow-300 hover:bg-yellow-200 focus:outline-none focus:bg-yellow-300 focus:ring focus:ring-yellow-200 focus:ring-opacity-50"
+                                    class="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-150  text-white bg-blue-500 hover:bg-blue-400 focus:ring-4 focus:ring-blue-300 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center fond-semibold "
                                     data-modal-toggle="authentication-modal" type="submit"
                                     value="Envoyer un mot de passe temporaire">
                             </div>
