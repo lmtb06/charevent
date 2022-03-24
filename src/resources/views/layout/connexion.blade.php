a89b201ba3cb2b5ed0cf8d44a9395d93592baf3d
Romy <romane.piing@gmail.com>
2022-03-24 22:24:04 +0100

    Lié bouton profil du bandeau front/back

--- src/resources/views/layout/connexion.blade.php
+++ src/resources/views/layout/connexion.blade.php
@@ -58,7 +58,7 @@
 
 								<input type="password" name="password" id="password" placeholder="mot de passe" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
 							</div>
-
+					<div id="ERREUR" class="text-red-700"> </div>
 							<div class="mt-6">
 								<input type="submit" class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50" value="Connexion" />
 							</div>
@@ -74,6 +74,11 @@
 		@yield('content')
 	</div>
 
+	<script>
+		function ErreurConnection() {
+		document.getElementById('ERREUR').innerHTML = 'L\'adresse mail ou le mot de passe n\'est pas valide';
+        }
+	</script>
 </body>
 
 </html>
