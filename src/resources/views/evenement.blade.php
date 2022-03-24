1414232d5c8e98cf1cd3927ef3098d63ba82bfcc
Guillaume V <guillaume.vautrin.it@gmail.com>
2022-03-24 16:43:25 +0100

    Evolution page événement

--- src/resources/views/evenement.blade.php
+++ src/resources/views/evenement.blade.php
@@ -5,152 +5,157 @@
 @endsection
 
 @section('content')
-    
-
-    <body class=" bg-orange-200 ">
-        
+    <body class="">
         <main>
-            <!-- Bandeau en haut a ajouter-->
-            <section>
-
-            </section>
+            
             
             <!-- Bloc d'evenement-->
-            <section  class="p-4 flex items-center justify-center ">
+            <section  class="p-4  flex items-center justify-center">
                 
 
                 <!-- Le gros rectangle-->
-                <div class=" mt-2 border-2 border-black m-1 rounded-2xl bg-gradient-to-b from-white to-white-700 max-w-6xl ">
+                <div class="bg-white mt-2 border-2 border-black m-1 rounded-2xl  max-w-6xl">
                     <!-- Nom/date-->
-                    <div class="grid gap-1 grid-cols-3 mt-2 ml-5">
+                    <div class="grid gap-1 grid-cols-3 m-1 bg-gray-100">
+                        <div ></div>
                         <div class="px-4 flex items-center justify-center">
-                            <h2 class="text-2xl font-bold ml-3  mt-3">Début : {{$event->dateDebut}}</h2>
+                            <h2 class="text-3xl font-bold ml-3  mt-3">{{$event->titre}}</h2>
                         </div>
-                        <div class="px-4 flex items-center justify-center">
-                            <h2 class="text-2xl font-bold ml-3  mt-3">{{$event->titre}}</h2>
-                        </div>
-                        <div class="px-4 flex items-center justify-center">
-                            <h2 class="text-2xl font-bold ml-3  mt-3">Fin : {{$event->dateFin}}</h2>
-                        </div>
+                        <div ></div>
                     </div>
                     <!-- Image à mettre -->
-                    <div class="m-1 px-4 mt-5 flex items-center justify-center">
-                        <img class="" src="http://picsum.photos/id/10/500/400" alt="lac">
+                    <div class="w-full flex justify-center mt-5">
+                        <img class="" src="http://picsum.photos/id/10/600/400" alt="lac">
 
                     </div>
                     <!-- Description de l'évènement-->
-                    <div class="px-4 ">
+                    <div class="px-4 bg-gray-100 ">
                         <h2 class="text-2xl ml-3 mt-5 ">{{$event->description}}</h2>
                     </div>
-                    <!-- Titre besoin -->
-                    <div class="px-4">
-                        <h2 class="text-2xl font-bold ml-3 mt-5 ">Besoin :</h2>
-                    </div>
-                    <!-- Bloc Liste Besoin-->
-                    <div class="mt-5 ml-drop-shadow-2xl bg-gradient-to-b from-white to-white-500 m-1 border-2 border-black rounded-2xl">
-                       
-                       <!-- Liste Besoin -->
-                       <div class="grid px-1 grid-cols-2">
-                            <!--Petit bloc une liste a mettre en boucle-->
-                            <div class=" bg-gradient-to-b from-white to-white-500 m-1 border-2 border-black rounded-2xl">
-                                <!-- Un besoin -->
-                                <div class="m-1">
-                                    <h2 class="ml-5">Nom besoin</h2>
-                                </div>
-                            </div>
-                            <!-- Fin du Petit bloc une liste a mettre en boucle-->
-
-                            <!--2e Petit bloc une liste a mettre en boucle-->
-                            <div class=" bg-gradient-to-b from-white to-white-500 m-1 border-2 border-black rounded-2xl">
-                                <!-- Un besoin -->
-                                <div class="m-1">
-                                    <h2 class="ml-5">Nom besoin</h2>
-                                </div>
-                            </div>
-                            <!--2e  Fin du Petit bloc une liste a mettre en boucle-->
-                            
-                        </div>
-                        <div class="mt-2 flex items-center justify-center" >
-                            <button type="submit" class="text-ms rounded-2xl border-2 border-orange-800 bg-gradient-to-b from-orange-400 to-orange-100 m-2 p-2 ">
-                                    Ajouter un besoin
-                            </button>
-        
-                        </div>
-                    </div>
+                    
                     <!-- Titre Participants -->
-                    <div class="px-4">
-                        <h2 class="text-2xl font-bold ml-3 mt-5 ">Liste des participant :</h2>
+                    <div class=" sm:grid sm:grid-cols-2 sm:gap-10">
+                        <div class="px-4 flex items-center justify-center">
+                            <h2 class="text-2xl font-bold  mt-5 underline">Liste des participant </h2>
+                        </div>
+                        <div class="px-4 flex items-center justify-center">
+                                            <h2 class="text-2xl font-bold  mt-5 underline">Liste des Besoins : </h2>
+                        </div>
                     </div>
+                        
                     <!-- Bloc Listes des participants-->
-                    <div class="mt-5 ml-drop-shadow-2xl bg-gradient-to-b from-white to-white-500 m-1 border-2 border-black rounded-2xl">
+                    <div class="bg-white mt-5 ml-drop-shadow-2xl  m-1 border-5 border-indigo-800 rounded-2xl ">
                        
                         <!-- Liste Participants -->
-                        <div class="grid px-1 grid-cols-2">
-                            @if (count($event->comptes) < 1)
-                                Pas encore de participants
-                            @else
-                            @foreach ($event->comptes as $p)
-                             <!--Petit bloc une liste a mettre en boucle-->
-                             <div class=" bg-gradient-to-b from-white to-white-500 m-1 border-2 border-black rounded-2xl">
-                                 <!-- Un participant -->
-                                <a href="{{route('pageProfil', ['id' => $p->id_compte])}}">
-                                    <div class="m-1 grid px-1 grid-cols-2">
-                                        <div>
-                                            <h2 class="ml-5">{{$p->prenom}} {{$p->nom}}</h2>
+                        
+
+                       
+                                <div class="grid px-1 grid-cols-2 ">
+
+
+                                    <div class="overflow-y-auto h-64 ml-2 mr-2">
+                                        <!--Petit bloc une liste a mettre en boucle-->
+
+                                        @if (count($event->comptes) < 1)
+                                        <div class=" bg-indigo-100 m-1 border-4 border-indigo-400 rounded-2xl">
+                                            <!-- Un particiapant-->
+                                            <div class="bg-gray-50 px-4 py-1 sm:grid sm:gap-10 sm:px-6">
+                                                <dt class="text-sm font-medium mt-8 justify-center text-black-500 ">Il n'y a pas encore de participants...</dt>
+                                            </div>
                                         </div>
-                                    <div>
-                                        <h2 class="ml-5 flex items-center justify-center">{{$p->codePostal}}</h2>
-                                    </div>                                     
-                                 </div>
-                                </a>
-                             </div>
-                            @endforeach     
-                            @endif                        
-                         </div>
-                         
-                     </div>
+                                        
+                                        @else
+                                        @foreach ($event->comptes as $p)
+
+                                        <!--2e  Fin du Petit bloc une liste a mettre en boucle-->
+                                        <div class=" bg-indigo-100  m-1 border-4 border-indigo-400 rounded-2xl">
+                                            <!-- Un particiapant-->
+                                            <div class="bg-gray-50 px-4 py-1 sm:grid sm:grid-cols-4 sm:gap-10 sm:px-6">
+                                                <div class="mt-1 mb-1">
+                                                <img src="https://mdbootstrap.com//img/Photos/Square/1.jpg" class="w-20 h-auto rounded-full" alt="">
+                                                </div>
+                                                <dt class="text-sm font-medium mt-8 justify-center text-black-500 ">{{$p->nom}}</dt>
+                                                <dt class="text-sm font-medium mt-8 justify-center text-black-500">{{$p->prenom}}</dt>
+                                                <dt class="text-sm font-medium mt-8 justify-center text-black-500">{{$p->ville}}</dt>
+                                            </div>
+                                        </div>
+                                        @endforeach
+                                        @endif
+                                    </div>
+                                    <div class="overflow-y-auto h-64 ml-2 mr-2">
+                                        
+                                    @if (count($event->besoins) < 1)
+                                        <div class=" bg-indigo-100  m-1 border-4 border-indigo-400 rounded-2xl">
+                                            <!-- Un Besoin-->
+                                            <div class="bg-gray-50 px-4 py-1 sm:grid sm:grid-cols-2 sm:gap-10 sm:px-6">
+                                                <div class="mt-1 mb-1">
+                                                    <dt class="text-sm font-medium mt-3 justify-center text-black-500 ">Il n'y a pas encore de besoins...</dt>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @else
+                                    @foreach ($event->besoins as $b)
+                                        <div class=" bg-indigo-100  m-1 border-4 border-indigo-400 rounded-2xl">
+                                            <!-- Un Besoin-->
+                                            <div class="bg-gray-50 px-4 py-1 sm:grid sm:grid-cols-2 sm:gap-10 sm:px-6">
+                                                <div class="mt-1 mb-1">
+                                                    <dt class="text-sm font-medium mt-3 justify-center text-black-500 ">{{$b->titre}}</dt>
+                                                    
+                                                </div>
+                                                <div class=" justify-end flex">
+                                                <button class="underline  text-black font-bold py-2 px-4 my-2 ml-8" type="button" >Modifier</button>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                    @endif
+
+                                    </div>
+                            </div>
+                            <div class="sm:grid sm:grid-cols-2 sm:gap-10 sm:px-6">
+
+
+                                <div class="mt-2 flex items-center justify-center">
+                                    @if ($event->id_createur == Auth::id())
+                                        <a href="{{route('pageFromRechercheParticipants', ['id' => $event->id_evenement])}}">
+
+                                            <button type="submit" class="modal-open bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded my-2 ">
+                                            Inviter des personnes
+                                            </button>
+                                        </a>
+                                    @else
+                                    <form method="POST" action="{{route('postule', ['id' =>  $event->id_evenement])}}">
+                                        @csrf
+                                        <input type="submit" class="modal-open bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded my-2 "
+                                            value="Postuler"
+                                            @if (!is_null($demande))
+                                                @if (!isset($demande->accepte) && !isset($demande->dateChoix))
+                                                    hidden disabled
+                                                @endif
+                                            @endif
+                                        />
+                                    @endif
+                                </div>
+                                
+                                <div class="mt-2 flex items-center justify-center" >
+                                        <button class="modal-open bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded my-2" type="button" >Ajouter un besoin</button>
+                                </div>
+                            </div>
+                            <div class="grid gap-1 grid-cols-3 mt-2 ml-5 bg-gray-100">
+                                <div class=" flex items-center justify-center">
+                                    <h2 class="text-2xl ">Début : {{$event->dateDebut}}</h2>
+                                </div>
+                                <div ></div>
+                                <div class=" flex items-center justify-center">
+                                    <h2 class="text-2xl">Fin : {{$event->dateFin}}</h2>
+                                </div>
+                            </div>
+                        </div> 
+                    </div>
                 </div>
 
             </section>
-
-            <section class="mt-2 flex items-center justify-center" >
-                <!-- Bouton Inviter à l'évènement -->
-                <div>
-                    @if ($event->id_createur == Auth::id())
-                        <a href="{{route('pageFromRechercheParticipants', ['id' => $event->id_evenement])}}">
-                        <button type="submit"
-                        class="text-ms rounded-2xl border-2 border-orange-800 bg-gradient-to-b from-orange-400 to-orange-100 m-2 p-2 ">
-                                Inviter des participants
-                        </button>
-                        </a>
-
-                        <!-- Bouton pour modifier l'événement -->
-
-                        <a href="{{route('pageModificationEvenement', ['id' => $event->id_evenement])}}">
-                        <button type="submit"
-                        class="text-ms rounded-2xl border-2 border-orange-800 bg-gradient-to-b from-orange-400 to-orange-100 m-2 p-2 ">
-                                Modifier l'événement
-                        </button>
-                        </a>
-
-                    @else
-                    <form method="POST" action="{{route('postule', ['id' =>  $event->id_evenement])}}">
-                        @csrf
-                        <input type="submit"
-                        class="text-ms rounded-2xl border-2 border-orange-800 bg-gradient-to-b from-orange-400 to-orange-100 m-2 p-2 "
-                        value="Postuler"
-                        @if (!is_null($demande))
-                            @if (!isset($demande->accepte) && !isset($demande->dateChoix))
-                                hidden disabled
-                            @endif
-                        @endif
-                        />
-                    </form>
-
-                    @endif
-                </div>
-            </section>
-            
             
         </main>
 
