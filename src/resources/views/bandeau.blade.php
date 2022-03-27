55d081a261e56b141ca16065f951389450c89909
Romy <romane.piing@gmail.com>
2022-03-27 16:03:48 +0200

    navigation barre

--- src/resources/views/bandeau.blade.php
+++ src/resources/views/bandeau.blade.php
@@ -17,6 +17,9 @@
                 <a href="{{route('accueil')}}" CharEvent><img src="https://github.com/lmtb06/charevent/blob/main/src/resources/img/logo3.png?raw=true" class=" h-14" alt="CharEvent"  /> </a>
                 <span class="self-center text-xl font-semibold whitespace-nowrap text-center text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-blue-600"><a href="{{route('accueil')}}" CharEvent>CharEvent</span>
         </div>
+
+
+
             <ul class="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                 
                 <li>
@@ -31,33 +34,10 @@
                    
                 </li>
             </ul>
+
+            
     </div>
 
-
-            <!--Barre de recherche-->
-            <section class="p-4 flex items-center justify-center">
-                <div class="  bg-orange-200 flex items-start">
-                    <div class="bg-orange-200  text-gray-300">
-                        <div class="relative bg-white p-2 rounded-3xl border-2 border-orange-800">
-                            <svg class="h-6 w-5 absolute left-0 ml-2" fill="black" viewBox="0 0 20 20">
-                                <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd">
-                                </path>
-                            </svg>
-                            <input type="text" placeholder="Search..." class="ml-6 bg-transparent text-black">
-    
-                        </div>
-                    </div>
-                    <div class="bg-orange-200 flex items-center justify-center text-gray-300">
-                        <div class="relative p-2 ">
-                                <svg  class="h-7 w-8 absolute left-0 pointer-events-none" fill="white" viewBox="0 0 24 24" stroke="black" strokeWidth={2}>
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                                </svg></a>
-                        </div>
-                    </div>
-                </div>
-                </section>
-                <!--Barre de recherche FIN-->
 
 
     <ul>
