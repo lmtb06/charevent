f3ebf1cd9db57c07a5808bfcf811bc7d345085dc
Romy <romane.piing@gmail.com>
2022-03-27 19:54:04 +0200

    vues changements

--- src/resources/views/bandeau.blade.php
+++ src/resources/views/bandeau.blade.php
@@ -11,11 +11,11 @@
     </script>
 
 
-<nav class="">
-    <div class=" flex flex-wrap h-16 w-full justify-between items-center mx-auto bg-white border-bg-orange-300 shadow-lg shadow-red-500 md:shadow-xl fixed inset-x-0">
+<nav>
+    <div class="shadow-lg z-40 shadow-[#6f6ba531] flex flex-wrap h-16 w-full justify-between items-center mx-auto bg-white fixed inset-x-0">
         <div class="flex items-center">
                 <a href="{{route('accueil')}}" CharEvent><img src="https://github.com/lmtb06/charevent/blob/main/src/resources/img/logo3.png?raw=true" class=" h-12 lg:h-14" alt="CharEvent"  /> </a>
-                <span class="self-center text-lg lg:text-2xl font-semibold whitespace-nowrap text-center text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-blue-600"><a href="{{route('accueil')}}" CharEvent>CharEvent</span>
+                <span class="self-center text-lg lg:text-2xl font-semibold whitespace-nowrap text-center text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-blue-400"><a href="{{route('accueil')}}" CharEvent>CharEvent</span>
         </div>
 
 
@@ -25,7 +25,7 @@
                 <li>
                     <button onclick="">
                     <a href="{{route('pageProfil')}}">
-                        <img src="https://github.com/lmtb06/charevent/blob/main/src/public/img/2.png?raw=true" class="mr-4 lg:mr-8 h-8 lg:h-10" alt="CharEvent" />
+                        <img src="https://github.com/lmtb06/charevent/blob/main/src/public/img/2.png?raw=true" class=" rounded-full mr-4 lg:mr-8 h-8 lg:h-10" alt="CharEvent" />
                     </a></button> 
                     
                     <button onclick="myFunction()">
@@ -41,7 +41,7 @@
 
 
     <ul>
-        <div id="menu_perso" style="display:none; " class="mt-20 fixed z-40 mr-3 origin-top-right right-0 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+        <div id="menu_perso" style="display:none; " class="fixed z-40 lg:mt-20 lg:mr-2 mt-16 origin-top-right right-0 w-48 shadow-[#6f6ba56e] rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
             <!-- Active: "bg-gray-100", Not Active: "" -->
             <li><a href="{{route('pageProfil')}}" class="block px-4 py-2 text-xl  hover:text-purple-400 text-indigo-500 " role="menuitem" tabindex="-1" id="user-menu-item-0">Mon Profil</a></li>
             <li><a href="#" class="block px-4 py-2 text-xl hover:text-purple-400 text-indigo-500 " role="menuitem" tabindex="-1" id="user-menu-item-1">Mes Evénements</a></li>
